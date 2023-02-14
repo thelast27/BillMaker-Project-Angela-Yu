7f0bd60d57338b9169b41257b2ac3694a01fb5f2
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2023-02-14 10:06:48 +0300

    add calculation logic and showing on result VC

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,20 +16,27 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var spritNumberButton: UILabel!
     
-  private var tip = 0.10
+    private var tip: Double = 0.10
+    private var pctCurrentValue: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pctCurrentValue = "10"
+    }
     
     @IBAction func tipPctChanged(_ sender: UIButton) {
-        
+        sumTextField.endEditing(true)
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
         sender.isSelected = true
         
+        
         guard let buttonTitle = sender.currentTitle else { return }
         let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
         guard let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign) else { return }
         tip = buttonTitleAsANumber / 100
-      
+        pctCurrentValue = buttonTitleMinusPercentSign
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
@@ -37,6 +44,17 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        guard let totalBil = Double(sumTextField.text ?? ""), let split = Double(spritNumberButton.text ?? ""), let pctValue = Double(pctCurrentValue ?? "") else { return }
+        let result = Float((totalBil + tip) / split)
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else { return }
+        vc.totalResult = result
+        vc.totalPeople = Int(spritNumberButton.text ?? "")
+        vc.totalPct = Double(pctValue)
+        
+        self.present(vc, animated: true, completion: nil)
+        
+        
     }
     
 }
